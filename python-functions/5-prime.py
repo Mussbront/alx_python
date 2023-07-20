98db7e9ddd4bda1dee8f2d32f185dd199aa76a69
Mussbront <vmogendi@gmail.com>
2023-07-20 22:09:39 +0300

    Prime numbers

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, number):
+         if number % i == 0:
+            return False
+         else:
+             return True
