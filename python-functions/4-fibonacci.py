f3ba9a5c19a19ef40d689674539cc74ca426622a
Mussbront <vmogendi@gmail.com>
2023-07-20 21:56:52 +0300

    Fibonacci sequence

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+        sequence = [0, 1]
+        while len(sequence) < n:
+            next_number = sequence[-1] + sequence[-2]
+            sequence.append(next_number)
+    return sequence
