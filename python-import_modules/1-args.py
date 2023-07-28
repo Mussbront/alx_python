d509a5f208fd7f2effe555b7d72d390bae29d4d2
Mussbront <vmogendi@gmail.com>
2023-07-29 00:47:40 +0300

    args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    if num_arguments == 1:
+        print("{}: argument".format(num_arguments), end="\n")
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(num_arguments, arguments), end="\n")
+    elif num_arguments > 1:
+        print("{}: arguments".format(num_arguments), end="\n")
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(num_arguments, arg), end="\n")
+    elif num_arguments == 0:
+        print("{}: arguments".format(num_arguments), end="\n")
+        print(".", end="\n")
+
+if __name__ == "__main__":
+    main()
