53cb458fe4b1b19d1b3220126bc44cf33e25ced7
Mussbront <vmogendi@gmail.com>
2023-08-01 16:03:39 +0300

    try, except, & finally

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result = a / b
-    print("{:d} / {:d} = {}".format(a, b, result))
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(result), end="\n")
+        print("{:d} / {:d} = {}".format(a, b, result), end="\n")
+    
+safe_print_division(8, 0)
