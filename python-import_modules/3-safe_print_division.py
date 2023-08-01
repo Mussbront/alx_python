5d66fc08c562127d10cc435633eba3e20e3b4a89
Mussbront <vmogendi@gmail.com>
2023-08-01 16:06:50 +0300

    removed function call

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,5 +9,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result), end="\n")
         print("{:d} / {:d} = {}".format(a, b, result), end="\n")
-    
-safe_print_division(8, 0)
