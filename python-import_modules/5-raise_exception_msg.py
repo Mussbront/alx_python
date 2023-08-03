4b44e280a14277627b06ab33c4d19716f5f70126
Mussbront <vmogendi@gmail.com>
2023-08-04 00:00:46 +0300

    removed print

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -4,6 +4,5 @@ def raise_exception_msg(message=""):
     raise NameError(message)
 try:
     raise_exception_msg(message="")
-except NameError as ne:
-    raise ne
-print(message)
+except NameError as e:
+    raise e
