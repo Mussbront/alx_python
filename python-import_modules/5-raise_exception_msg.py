3ec7cf88451f552ed9d5208e51f56ad4b6910088
Mussbront <vmogendi@gmail.com>
2023-08-04 00:25:50 +0300

    removed message= from line 6

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,6 +3,6 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
 try:
-    raise_exception_msg(message="")
-except NameError as ne:
-    print(ne)
+    raise_exception_msg()
+except NameError as e:
+    print(e)
