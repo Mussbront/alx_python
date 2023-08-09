b81f6ba874b2cbce031f6ca312c30e1decbede5e
Mussbront <vmogendi@gmail.com>
2023-08-09 19:42:38 +0300

    removed if not statement

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print(" ")
-        return
-    else:
-        for row in matrix:
-            for i, num in enumerate(row):
-                if i == len(row) - 1:
-                    print("{:d}".format(num), end="\n")
-                else:
-                    print("{:d}".format(num), end=" ")
+    for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
+            else:
+                print("{:d}".format(num), end=" ")
+        print()
